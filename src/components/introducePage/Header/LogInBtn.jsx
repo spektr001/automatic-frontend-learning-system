d58aeb1557cc91318c0de330f128a1c17aa3372d
spektr001 <sanek777.28.03.2000@gmail.com>
2021-04-01 16:48:41 +0300

    modal window ready

--- src/components/introducePage/Header/LogInBtn.jsx
+++ src/components/introducePage/Header/LogInBtn.jsx
@@ -6,33 +6,64 @@ import clsObj from './header.module.scss'
 export class LogInBtn extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            isRedirect: false
-         }
+        this.state = {
+            modalOpen: false,
+            isRedirect: false,
+            name: ""
+        }
 
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState({isRedirect: true})
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     redirect() {
         if (this.state.isRedirect) {
-          return (
-            <Redirect to="/dashboard" />
-          )
+            return (
+                <Redirect to="/dashboard" />
+            )
         }
-      }
+    }
 
-    render() { 
-        return ( 
-            <div className={clsObj.headerNav__Block}>
+    handleChange(e) {
+        const name = e.target.name
+        this.setState({ [name]: e.target.value });
+    }
+
+    handleSubmit() {
+        localStorage.setItem('name', this.state.name);
+        this.setState({ isRedirect: true })
+    }
+
+    render() {
+        return (
+            <>
+                <a className="user-block_add-btn" onClick={() => this.setState({ modalOpen: true })}>
+                    <img src={LogIn} alt="Login" className={clsObj.logIn__img} />
+                </a>
                 {this.redirect()}
-                    <a className={clsObj.logIn__btn} onClick={this.handleClick}>
-                        <img src={LogIn} alt="Login" className={clsObj.logIn__img} />
-                    </a>
-                </div>
-         );
+                {this.state.modalOpen && (
+                    <div>
+                        <div className={clsObj.modalFilter}>
+                            <form onSubmit={this.handleSubmit} className={clsObj.modalFilter__Block}>
+                                <p className={clsObj.modal_title}>Введи своє ім'я</p><br />
+                                <label className={clsObj.modal_label}>Імя:
+                                <input
+                                        onChange={this.handleChange}
+                                        className={clsObj.modal_input}
+                                        type="text"
+                                        name="name"
+                                        value={this.state.name}
+                                    />
+                                </label>
+                                <div className={clsObj.modal_btn__Block}>
+                                    <button className={clsObj.modal_btn} type="submit">Підтвердити</button>
+                                    <button className={clsObj.modal_btn} onClick={() => this.setState({ modalOpen: false })}>Відмінити</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                )}
+            </>
+        );
     }
 }
